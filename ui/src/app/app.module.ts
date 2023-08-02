c5f8d0b24115bd7a62161b6006a5d4019d7f2683
lmesi <lszl.emese@gmail.com>
2023-08-02 14:32:49 +0200

    feat(ui): add home and top-bar component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
   ],
+  declarations: [AppComponent, HomeComponent, TopBarComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
