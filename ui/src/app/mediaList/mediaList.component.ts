2d916e73e4293d3fc569cd969b89f5b4320be8fa
lmesi <lszl.emese@gmail.com>
2023-08-03 14:29:03 +0200

    feat(ui): add series to mediaList component

--- ui/src/app/mediaList/mediaList.component.ts
+++ ui/src/app/mediaList/mediaList.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { movies } from 'src/assets/movies';
+import { Component, NgIterable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Media, movies } from 'src/assets/movies';
+import { series } from 'src/assets/series';
 
 @Component({
   selector: 'app-mediaList',
   templateUrl: './mediaList.component.html',
   styleUrls: ['./mediaList.component.css'],
 })
-export class MediaListComponent {
-  movies = [...movies];
+export class MediaListComponent implements OnInit {
+  medias!: Array<Media> | undefined | null;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const url = this.router.url;
+
+    if (url === '/movies') {
+      this.medias = [...movies];
+    } else if (url === '/series') {
+      this.medias = [...series];
+    }
+    //fall back else
+  }
 }
