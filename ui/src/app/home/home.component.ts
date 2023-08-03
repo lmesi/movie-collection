f7ebea7c82eb52e980664ea84f2d4eea2a61df09
lmesi <lszl.emese@gmail.com>
2023-08-03 19:27:46 +0200

    chore(ui): change posters in home component

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { movies } from 'src/assets/movies';
+import { series } from 'src/assets/series';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {}
+export class HomeComponent {
+  movies = [...movies];
+  series = [...series];
 
   numSequence(n: number): Array<number> {
     return Array(n);
