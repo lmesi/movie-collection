3e74ea1f5c37104725840bf99590c35802b8af07
lmesi <lszl.emese@gmail.com>
2023-08-14 16:13:00 +0200

    feat(server): add server skeleton

---
+++ server/src/test/java/com/server/moviecollection/MovieCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.moviecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
