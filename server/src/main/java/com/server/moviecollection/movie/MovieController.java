35d562118d923b089ff43f3edd845c1d1509d5f3
lmesi <lszl.emese@gmail.com>
2023-08-16 19:45:58 +0200

    feat(server): add Movie and Series controller

---
+++ server/src/main/java/com/server/moviecollection/movie/MovieController.java
@@ -0,0 +1,36 @@
+package com.server.moviecollection.movie;
+
+import com.server.moviecollection.collection.Collection;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    private MovieRepository movieRepository;
+
+    public MovieController (MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+
+//        movieRepository.save(new Movie("Shrek", "/assets/Shrek.jpg", 2001, new String[]{"Andrew Adamson", "Vicky Jenson"}, "1h 30m", false, true));
+//        movieRepository.save(new Movie("Forrest Gump", "/assets/Forrest_Gump.jpg", 1994, new String[]{"Robert Zemeckis"}, "2h 22m", false, false));
+//        movieRepository.save(new Movie("Fight Club", "/assets/Fight_Club.jpg", 1999, new String[]{"David Fincher"}, "2h 19m", false, true));
+//        movieRepository.save(new Movie("The Matrix", "/assets/The_Matrix.jpg", 1999, new String[]{"Lana Wachowski", "Lilly Wachowski"}, "2h 16m", false, false));
+//        movieRepository.save(new Movie("Inception", "/assets/Inception.jpg", 2010, new String[]{"Christopher Nolan"}, "2h 28m", false, true));
+    }
+
+    @GetMapping("/api/movies")
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    @GetMapping("/api/movies/{id}")
+    public ResponseEntity<Object> getMovieById(@PathVariable int id){
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if(movie == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(movie);
+    }
+}
---
+++ server/src/main/java/com/server/moviecollection/series/SeriesController.java
@@ -0,0 +1,37 @@
+package com.server.moviecollection.series;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+public class SeriesController {
+
+    private SeriesRepository seriesRepository;
+
+    public SeriesController(SeriesRepository seriesRepository){
+        this.seriesRepository = seriesRepository;
+
+//        seriesRepository.save(new Series("Friends", "/assets/Friends.jpg", "1994-2004", new String[]{"David Crane", "Marta Kauffman"}, "20m", 234, 10, true, true));
+//        seriesRepository.save(new Series("Rick and Morty", "/assets/Rick_and_Morty.jpg", "2013-Nowdays", new String[]{"Dan Harmon", "Justin Roiland"}, "23m", 63, 6, false, false));
+//        seriesRepository.save(new Series("The Office", "/assets/The_Office.jpg", "2005-2013", new String[]{"Greg Daniels", "Ricky Gervais", "Stephen Merchant"}, "22m", 188, 9, false, true));
+//        seriesRepository.save(new Series("House M.D.", "/assets/House_MD.jpg", "2004-2012", new String[]{"David Shore"}, "44m", 176, 8, false, true));
+//        seriesRepository.save(new Series("Stranger Things", "/assets/Stranger_Things.jpg", "2016-2024", new String[]{"Matt Duffer", "Ross Duffer"}, "51m", 42, 5, false, true));
+    }
+
+    @GetMapping("/api/series")
+    public List<Series> getAllSeries(){
+        return seriesRepository.findAll();
+    }
+
+    @GetMapping("/api/series/{id}")
+    public ResponseEntity<Object> getSeriesById(@PathVariable int id){
+        Series series = seriesRepository.findById(id).orElse(null);
+        if(series == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(series);
+    }
+}
