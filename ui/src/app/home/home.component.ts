03f6c9d6263607dd8ebaea75603e6c5f436f2d92
lmesi <lszl.emese@gmail.com>
2023-08-17 20:04:44 +0200

    feat(ui): add server data usage to home component

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { movies } from 'src/assets/movies';
-import { series } from 'src/assets/series';
+import { Movie, Series } from '../medias.model';
+import { MediaService } from '../media.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  movies = [...movies];
-  series = [...series];
+export class HomeComponent implements OnInit {
+  movies!: Movie[];
+  series!: Series[];
+
+  constructor(private mediaService: MediaService) {}
+
+  ngOnInit(): void {
+    this.mediaService.getMovies().subscribe((data) => {
+      this.movies = data;
+    });
+
+    this.mediaService.getSeries().subscribe((data) => {
+      this.series = data;
+    });
+  }
 
   numSequence(n: number): Array<number> {
     return Array(n);
