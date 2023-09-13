f743f734a2118171848bf77e5800fa140d369809
lmesi <lszl.emese@gmail.com>
2023-09-13 17:45:58 +0200

    feat(ui): add addToCollection drop down to series-details component

--- ui/src/app/series-details/series-details.component.ts
+++ ui/src/app/series-details/series-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Series } from 'src/app/medias.model';
 import { MediaService } from '../media.service';
+import { CollectionService } from '../collection.service';
+import { CollectionBasic } from '../collection.model';
 
 @Component({
   selector: 'app-series-details',
@@ -10,16 +12,35 @@ import { MediaService } from '../media.service';
 })
 export class SeriesDetailsComponent implements OnInit {
   series!: Series | undefined;
+  seriesId!: number;
+  collectionOptions!: CollectionBasic[];
+  selectedCollection: number | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private mediaSerivce: MediaService
+    private router: Router,
+    private mediaSerivce: MediaService,
+    private collectionService: CollectionService
   ) {}
 
   ngOnInit(): void {
-    const { seriesId } = this.route.snapshot.params;
-    this.mediaSerivce.getSeriesById(seriesId).subscribe((data) => {
+    this.seriesId = this.route.snapshot.params['seriesId'];
+    this.mediaSerivce.getSeriesById(this.seriesId).subscribe((data) => {
       this.series = data;
     });
+    this.collectionService.getCollections().subscribe((data) => {
+      this.collectionOptions = data;
+    });
+  }
+
+  addToCollection() {
+    if (this.selectedCollection !== undefined)
+      this.collectionService
+        .addToCollection(this.seriesId, this.selectedCollection, 'series')
+        .subscribe(() => {
+          this.router.navigate(['/collections', this.selectedCollection], {
+            relativeTo: this.route.root,
+          });
+        });
   }
 }
