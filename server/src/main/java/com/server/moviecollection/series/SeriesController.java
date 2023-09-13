c20e7c83dacca1ebb90369de3837d02a072c9cb4
lmesi <lszl.emese@gmail.com>
2023-09-13 13:40:00 +0200

    refactor(server): remove unused imports

--- server/src/main/java/com/server/moviecollection/series/SeriesController.java
+++ server/src/main/java/com/server/moviecollection/series/SeriesController.java
@@ -1,6 +1,5 @@
 package com.server.moviecollection.series;
 
-import com.server.moviecollection.movie.Movie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 @RestController
