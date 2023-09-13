1e3bf8237a618b2351ac7f492321dbb3c6bf2085
lmesi <lszl.emese@gmail.com>
2023-09-13 17:35:58 +0200

    feat(ui): add formControl, validation and style to mediaList component

--- ui/src/app/mediaList/mediaList.component.ts
+++ ui/src/app/mediaList/mediaList.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Media } from 'src/app/medias.model';
 import { MediaService } from '../media.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mediaList',
@@ -15,6 +16,13 @@ export class MediaListComponent implements OnInit {
   searchYear: number | undefined;
   maxYear = new Date().getFullYear() + 5;
   isYearValid: boolean = true;
+  searchForm = new FormGroup({
+    title: new FormControl(undefined, [Validators.minLength(2)]),
+    year: new FormControl(undefined, [
+      Validators.min(1900),
+      Validators.max(this.maxYear),
+    ]),
+  });
 
   constructor(private router: Router, private mediaService: MediaService) {}
 
@@ -32,18 +40,25 @@ export class MediaListComponent implements OnInit {
         this.type = 'series';
       });
     }
-    //fall back else
   }
 
   handleSearch() {
-    if (this.searchYear !== undefined)
-      this.isYearValid = this.searchYear > 1900;
-
-    if (this.isYearValid)
+    if (
+      this.searchForm.valid &&
+      (this.searchForm.value.title || this.searchForm.value.year)
+    )
       this.mediaService
-        .searchForMedias(this.type, this.searchTitle, this.searchYear ?? 0)
+        .searchForMedias(
+          this.type,
+          this.searchForm.value.title ?? '',
+          this.searchForm.value.year ?? 0
+        )
         .subscribe((data) => {
           this.medias = data;
         });
+  }
+
+  refreshPage() {
+    window.location.reload();
   }
 }
