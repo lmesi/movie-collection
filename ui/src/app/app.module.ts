dd0ce8cf2b30e231e74b076729f73c66d8effeb3
lmesi <lszl.emese@gmail.com>
2023-09-13 17:24:59 +0200

    chore: add @angular/material

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,8 +14,10 @@ import { MediaListComponent } from './mediaList/mediaList.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { SeriesDetailsComponent } from './series-details/series-details.component';
 import { CollectionComponent } from './collection/collection.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CollectionDetailsComponent } from './collection-details/collection-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -28,6 +34,7 @@ import { CollectionDetailsComponent } from './collection-details/collection-deta
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'movies', component: MediaListComponent },
@@ -39,7 +46,14 @@ import { CollectionDetailsComponent } from './collection-details/collection-deta
         path: 'collections/:collectionId',
         component: CollectionDetailsComponent,
       },
+      { path: '**', redirectTo: '' },
     ]),
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
