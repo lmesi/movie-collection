e12b0e88aa08054214a0655d5e3171c10aa78934
lmesi <lszl.emese@gmail.com>
2023-10-10 17:49:43 +0200

    feat(ui_react): add types

--- ui_react/src/types.ts
+++ ui_react/src/types.ts
@@ -1,5 +1,42 @@
-export type Media = {
+export type MediaType = {
   id: number;
   title: string;
   posterUrl: string;
+};
+
+export type MovieType = {
+  id: number;
+  title: string;
+  posterUrl: string;
+  year: number;
+  directors: Array<string>;
+  length: string;
+  favourite: boolean;
+  watched: boolean;
+};
+
+export type SeriesType = {
+  id: number;
+  title: string;
+  posterUrl: string;
+  startYear: number;
+  endYear: number;
+  directors: Array<string>;
+  length: string;
+  episodes: number;
+  seasons: number;
+  favourite: boolean;
+  watched: boolean;
+};
+
+export type CollectionBasicType = {
+  id: number;
+  title: string;
+};
+
+export type CollectionType = {
+  id: number;
+  title: string;
+  movies: MovieType[];
+  series: SeriesType[];
 };
