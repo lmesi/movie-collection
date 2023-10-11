ce26e46dceefc31714a6caf6c90e0c44257b7233
lmesi <lszl.emese@gmail.com>
2023-10-11 17:33:56 +0200

    feat(ui_react):add add new collection function

--- ui_react/src/Pages/Collections.tsx
+++ ui_react/src/Pages/Collections.tsx
@@ -1,14 +1,50 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { CollectionType } from "../types";
 import Card from "../Components/Card";
 import CardSvg from "../assets/checklist.svg";
+import { TextField, Button } from "@mui/material";
+import { ChangeEventHandler, useState } from "react";
 
 const Collections = () => {
   const collections: CollectionType[] = useLoaderData() as CollectionType[];
+  const [collectionTitle, setCollectionTitle] = useState("");
+  const [isTitleValid, setIsTitleValid] = useState(true);
+  const navigate = useNavigate();
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (event.target.value.length > 4) {
+      setIsTitleValid(true);
+    } else {
+      setIsTitleValid(false);
+    }
+    setCollectionTitle(event.target.value);
+  };
+
+  const handleAddition = () => {
+    fetch("/api/collections", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: collectionTitle,
+      }),
+    }).then(() => {
+      navigate(0);
+      setCollectionTitle("");
+    });
+  };
 
   return (
     <>
       <h1>Collections</h1>
+      <TextField
+        label="Collection name"
+        variant="outlined"
+        value={collectionTitle}
+        onChange={handleChange}
+        error={!isTitleValid}
+        helperText={isTitleValid ? undefined : "Incorrect entry."}
+      />
+      <Button onClick={handleAddition}>Add new collection</Button>
       <div className="cards_container">
         {collections.map((collection) => (
           <Card
