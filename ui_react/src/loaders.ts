8a46025011cdefc11df7c89bcb7aed5ae1a0d939
lmesi <lszl.emese@gmail.com>
2023-10-11 13:18:48 +0200

    feat(ui_react): add details loaders

--- ui_react/src/loaders.ts
+++ ui_react/src/loaders.ts
@@ -25,15 +25,46 @@ const movieLodaer = async (): Promise<MovieType[]> => {
   return await (await fetch("/api/movies")).json();
 };
 
+const movieDetailsLoader = async (id: string): Promise<MovieType> => {
+  if (id) {
+    return await (await fetch(`/api/movies/:${id}`)).json();
+  }
+  return await new Promise(() => []);
+};
+
 const seriesLoader = async (): Promise<SeriesType[]> => {
   return await (await fetch("/api/series")).json();
+};
+
+const seriesDetailsLoader = async (id: string): Promise<SeriesType> => {
+  if (id) {
+    return await (await fetch(`/api/series/:${id}`)).json();
+  }
+  return new Promise(() => []);
 };
 
 const collectionsLoader = async (): Promise<CollectionType> => {
   return await (await fetch("/api/collections")).json();
 };
 
-export { homeLoader, movieLodaer, seriesLoader, collectionsLoader };
+const collectionDetailsLoader = async (
+  id: string | undefined
+): Promise<CollectionType> => {
+  if (id) {
+    return await (await fetch(`/api/collections/${id}`)).json();
+  }
+  return await new Promise(() => []);
+};
+
+export {
+  homeLoader,
+  movieLodaer,
+  seriesLoader,
+  collectionsLoader,
+  movieDetailsLoader,
+  seriesDetailsLoader,
+  collectionDetailsLoader,
+};
 
 /**
  * TODO
