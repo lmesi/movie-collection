7cb539087e3953e8cae3c1e277cbad12aba977ef
lmesi <lszl.emese@gmail.com>
2023-10-17 18:25:18 +0200

    style(ui_react): style Media page

--- ui_react/src/Pages/Media.tsx
+++ ui_react/src/Pages/Media.tsx
@@ -3,6 +3,7 @@ import { Form, Field } from "react-final-form";
 import Card from "../Components/Card";
 import { MovieType, SeriesType } from "../types";
 import "./Media.css";
+import { TextField, Button } from "@mui/material";
 
 type IValues = {
   searchTitle?: string;
@@ -20,27 +21,47 @@ const Media = () => {
   };
 
   return (
-    <>
+    <div className="media-container">
       <h1>{mediaType}</h1>
       <Form
         onSubmit={onSubmit}
         render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <>
-              <label>Search for Title: </label>
-              <Field name="searchTitle" component="input" type="text" />
-            </>
-            <>
-              <label>Search for Year: </label>
-              <Field
-                name="searchYear"
-                component="input"
-                type="number"
-                min={1900}
-                max={maxYear}
-              />
-            </>
-            <button type="submit">Search</button>
+          <form onSubmit={handleSubmit} className="form-container">
+            <Field name="searchTitle">
+              {(props) => (
+                <TextField
+                  name={props.input.name}
+                  value={props.input.value}
+                  onChange={props.input.onChange}
+                  type="text"
+                  label="Search for Title"
+                  variant="outlined"
+                />
+              )}
+            </Field>
+
+            <Field name="searchYear" id="searchYear">
+              {(props) => (
+                <TextField
+                  name={props.input.name}
+                  value={props.input.value}
+                  onChange={props.input.onChange}
+                  label="Search for Year"
+                  variant="outlined"
+                  type="number"
+                  sx={{ width: 170, marginLeft: "25px" }}
+                  InputProps={{ inputProps: { min: 1900, max: maxYear } }}
+                />
+              )}
+            </Field>
+            <Button
+              id="searchBtn"
+              variant="outlined"
+              type="submit"
+              sx={{ marginLeft: "25px" }}
+            >
+              Search
+            </Button>
           </form>
         )}
       />
@@ -55,7 +76,7 @@ const Media = () => {
           />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
