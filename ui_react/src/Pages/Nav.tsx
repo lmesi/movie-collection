91dda4e86724dbc17778e2d99a72520afd46d1c4
lmesi <lszl.emese@gmail.com>
2023-10-17 15:23:06 +0200

    style(ui_react): style nav bar

--- ui_react/src/Pages/Nav.tsx
+++ ui_react/src/Pages/Nav.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 function Navbar() {
   return (
-    <>
+    <div className="top-bar-container">
       <Link to="/">Home</Link>
       <Link to="/collections">Collections</Link>
       <Link to="/movies">Movies</Link>
       <Link to="/series">Series</Link>
-    </>
+    </div>
   );
 }
 
